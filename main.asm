2680aa47a3d5c1d582e5556857c3b3041ce2e619
santi-raigoza <santiago.raigoza00@gmail.com>
2025-11-11 16:20:58 -0500

    Display and initialize cookie power and price
    Initializes and displays total cookies, cookie power level,
    and price to upgrade cookie power (initially set to 10).

--- main.asm
+++ main.asm
@@ -1,19 +1,37 @@
 INCLUDE Irvine32.inc
 
 .data
+    ; Game Variables
     cookieCount DWORD 0 ; Total Cookies
+    cookiePower DWORD 0 ; Cookies per Click
+    cookiePowerPrice DWORD 10 ; Price for Upgrading Cookie Power
+
+    ; Message Strings
     cookieMsg BYTE "Cookies: ", 0
+    cookiePowerMsg BYTE "Cookies Power: ", 0
+    cookiePowerPriceMsg BYTE "[1] Buy Cookie Power: ", 0
 
 .code
 main PROC
     ; Display total cookies
     mov edx, OFFSET cookieMsg
     call WriteString
-
-    ; Display the number stored in cookieCount
     mov eax, cookieCount
     call WriteDec
+    call Crlf
 
+    ; Display cookie power
+    mov edx, OFFSET cookiePowerMsg
+    call WriteString
+    mov eax, cookiePower
+    call WriteDec
+    call Crlf
+
+    ; Display cookie power upgrade price
+    mov edx, OFFSET cookiePowerPriceMsg
+    call WriteString
+    mov eax, cookiePowerPrice
+    call WriteDec
     call Crlf
     exit
 
