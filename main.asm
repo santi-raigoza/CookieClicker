b827c6a99e2163d403bab0fa6a0eb824a6f873d4
santi-raigoza <santiago.raigoza00@gmail.com>
2025-11-11 17:30:00 -0500

    Display and initialize auto cookie counter and controls
    Added an auto cookie variable and price set to 50. Displays
    auto cookie amount and price to purchase/upgrade.
    Added final message displaying available controls:
     [Spacebar] to add cookie, [Q] to quit.

--- main.asm
+++ main.asm
@@ -5,11 +5,16 @@ INCLUDE Irvine32.inc
     cookieCount DWORD 0 ; Total Cookies
     cookiePower DWORD 0 ; Cookies per Click
     cookiePowerPrice DWORD 10 ; Price for Upgrading Cookie Power
+    autoCookie DWORD 0 ; Auto Cookies per Second
+    autoCookiePrice DWORD 50 ; Price for Auto Cookie Generator
 
     ; Message Strings
     cookieMsg BYTE "Cookies: ", 0
     cookiePowerMsg BYTE "Cookies Power: ", 0
     cookiePowerPriceMsg BYTE "[1] Buy Cookie Power: ", 0
+    autoCookieMsg BYTE "Auto Cookies: ", 0
+    autoCookiePriceMsg BYTE "[2] Buy Auto Cookie: ", 0
+    controlMsg BYTE "[Spacebar] to add cookie, [Q] Quit Game", 0
 
 .code
 main PROC
@@ -33,6 +38,26 @@ main PROC
     mov eax, cookiePowerPrice
     call WriteDec
     call Crlf
+
+    ; Display auto cookie count
+    mov edx, OFFSET autoCookieMsg
+    call WriteString
+    mov eax, autoCookie
+    call WriteDec
+    call Crlf
+
+    ; Display auto cookie price
+    mov edx, OFFSET autoCookiePriceMsg
+    call WriteString
+    mov eax, autoCookiePrice
+    call WriteDec
+    call Crlf
+
+    ; Display control instructions
+    mov edx, OFFSET controlMsg
+    call WriteString
+    call Crlf
+
     exit
 
 main ENDP
